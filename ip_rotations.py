668e4b2d1aa2fbb3d10382879e8b92c3654254bd
mmuszik1 <74255809+mmuszik1@users.noreply.github.com>
2021-10-03 16:38:21 -0400

    Code for rotating IPs when sending http requests

---
+++ ip_rotations.py
@@ -0,0 +1,30 @@
+import requests
+from requests_ip_rotator import ApiGateway
+import pprint
+import pickle
+from bs4 import BeautifulSoup
+import json
+
+"""
+We want to be able to rotate IP addresses to prevent Zillow from blocking the web crawler.
+There are publicly available proxies that aren't very reliable because they're public.
+This method uses proxies from AWS API Gateway. Docs at https://github.com/Ge0rg3/requests-ip-rotator
+"""
+
+# Create gateway object and initialise in AWS
+gateway = ApiGateway("https://www.zillow.com")
+gateway.start()
+
+# Assign gateway to session
+session = requests.Session()
+session.mount("https://www.zillow.com", gateway)
+
+# Send request (IP will be randomised)
+url= 'https://www.zillow.com/homes/Knoxville-TN/'
+response = session.post(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+                                                                 "X-My-X-Forwarded-For":"29.391.493.29"})
+# We will also need to rotate the 'User-Agent'
+print(response.status_code)
+
+# Delete gateways
+gateway.shutdown()
