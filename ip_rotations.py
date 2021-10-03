d2354cd1a3c2213fdf768421b4bcbdf185139513
mmuszik1 <74255809+mmuszik1@users.noreply.github.com>
2021-10-03 16:44:30 -0400

    Update ip_rotations.py

--- ip_rotations.py
+++ ip_rotations.py
@@ -23,7 +23,7 @@ session.mount("https://www.zillow.com", gateway)
 url= 'https://www.zillow.com/homes/Knoxville-TN/'
 response = session.post(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
                                                                  "X-My-X-Forwarded-For":"29.391.493.29"})
-# We will also need to rotate the 'User-Agent'
+# We will also need to rotate the 'User-Agent' and maybe randomize the X-My-X-Fowarded-For
 print(response.status_code)
 
 # Delete gateways
