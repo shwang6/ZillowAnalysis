6b1a90f865cb692566fb610040d419e495e9e178
VishalAiely <vishalaiely29@gmail.com>
2021-10-17 17:43:01 -0400

    Minor fixes, formatting and renaming

--- parse_results.py
+++ parse_results.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import pprint
-
+import os
 
 #***************************#
 #    Search Results Page    #
@@ -12,37 +12,40 @@ This is the page that includes all the homes that resulted from a city/zip code 
 These pages can give us some general details on many properties with a single crawl. (40 properties per page)
 An example can be seen at https://www.zillow.com/knoxville-tn/
 """
-file = open("search-results-sample.txt", "r") 
+file = open(os.path.join('ExampleResponses', "search-results-sample.txt"), "r")
 response = file.read()
 file.close()
 soup = BeautifulSoup(response, "html.parser")
 
-#Get the number of listings
+# Get the number of listings
 nListings = soup.find_all("div", class_="total-text")
-print("There are", nListings[0].text, nListings[0].next_sibling, "and", nListings[1].text, nListings[1].next_sibling)
+print("There are", nListings[0].text, nListings[0].next_sibling,
+      "and", nListings[1].text, nListings[1].next_sibling)
 
-#Get data from all listings
-all_listings = soup.find('script', attrs={"data-zrr-shared-data-key": "mobileSearchPageStore"}).string
-#Remove comment symbols
+# Get data from all listings
+all_listings = soup.find(
+    'script', attrs={"data-zrr-shared-data-key": "mobileSearchPageStore"}).string
+# Remove comment symbols
 all_listings = all_listings.replace('<!--', '')
 all_listings = all_listings.replace('-->', '')
-#Load in JSON format
+# Load in JSON format
 all_listings = json.loads(all_listings)
 
-#Look at data:
-#View entire response
-pprint.pprint(all_listings) #Too much output to see it all
+# Look at data:
+# View entire response
+pprint.pprint(all_listings)  # Too much output to see it all
 
-#Narrow it down to just the results-related data
+# Narrow it down to just the results-related data
 all_listings.keys()
-all_listings['cat1'].keys() #We want 'cat1' > 'searchResults' > 'listResults'
-pprint.pprint(all_listings['cat1']['searchResults']['listResults']) #This is the file 'search-results-sample-cleaned.txt'
+all_listings['cat1'].keys()  # We want 'cat1' > 'searchResults' > 'listResults'
+# This is the file 'search-results-sample-cleaned.txt'
+pprint.pprint(all_listings['cat1']['searchResults']['listResults'])
 
-#This is a list of json dictionaries, one entry for each home result on the page
+# This is a list of json dictionaries, one entry for each home result on the page
 type(all_listings['cat1']['searchResults']['listResults'])
 len(all_listings['cat1']['searchResults']['listResults'])
-#There are 40 results per page. We'll need a way to detect the number of pages so we can cycle through them all.
-#NEXT STEPS:
+# There are 40 results per page. We'll need a way to detect the number of pages so we can cycle through them all.
+# NEXT STEPS:
 # Make sure we have all the variables we could possibly want to collect
 # Loop through all results from a single page and add variables to dataframe / database
 # Loop through all the results pages
@@ -56,19 +59,21 @@ It includes much more detail than the search results page, but these pages must 
 Not only will it make the function slower, but also increase the risk of the crawler getting blocked by Zillow.
 An example of this page can be found at https://www.zillow.com/homedetails/10301-Noras-Path-Ln-Knoxville-TN-37932/296376611_zpid/
 """
-file = open('individual-listing-sample.txt', 'r')
+file = open(os.path.join('ExampleResponses',
+            'individual-listing-sample.txt'), 'r')
 response = file.read()
 file.close()
 soup = BeautifulSoup(response, "html.parser")
 
-#Get data from listing
-data = json.loads(soup.find("script", type="application/json", id="hdpApolloPreloadedData").text)
+# Get data from listing
+data = json.loads(soup.find("script", type="application/json",
+                  id="hdpApolloPreloadedData").string)
 data = json.loads(data['apiCache'])
-pprint.pprint(data) #This is the file 'individual-list-sample-cleaned.txt'
+pprint.pprint(data)  # This is the file 'individual-list-sample-cleaned.txt'
 list(data.keys())[0]
 
 
-#Example attributes:
+# Example attributes:
 data['VariantQuery{"zpid":296376611,"altId":null}']['property'].keys()
 data['VariantQuery{"zpid":296376611,"altId":null}']['property']['yearBuilt']
 data['VariantQuery{"zpid":296376611,"altId":null}']['property']['lotSize']
@@ -80,8 +85,9 @@ data['VariantQuery{"zpid":296376611,"altId":null}']['property']['isZillowOwned']
 data['VariantQuery{"zpid":296376611,"altId":null}']['property']['taxAssessedValue']
 data['VariantQuery{"zpid":296376611,"altId":null}']['property']['lotAreaValue']
 data['VariantQuery{"zpid":296376611,"altId":null}']['property']['lotAreaUnit']
-data['VariantQuery{"zpid":296376611,"altId":null}']['property']['zestimate'] #This Zestimate does not match what's on the website for some reason
+# This Zestimate does not match what's on the website for some reason
+data['VariantQuery{"zpid":296376611,"altId":null}']['property']['zestimate']
 
-#NEXT STEPS:
+# NEXT STEPS:
 # Make sure we have all the variables we could possibly want to collect
 # Save all results from a single page and add to dataframe / database
--- working-scraper.py
+++ working-scraper.py
@@ -2,53 +2,56 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-headers= {
-            'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'accept-encoding':'gzip, deflate, sdch, br',
-            'accept-language':'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
-            'cache-control':'max-age=0',
-            'upgrade-insecure-requests':'1',
-            'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'referer':'https://www.google.com/'
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, sdch, br',
+    'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'referer': 'https://www.google.com/'
 }
-response = requests.get('https://www.zillow.com/homedetails/1723-Brent-Hills-Blvd-Gatlinburg-TN-37738/81444371_zpid/', headers=headers)
+response = requests.get(
+    'https://www.zillow.com/homedetails/1723-Brent-Hills-Blvd-Gatlinburg-TN-37738/81444371_zpid/', headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
+print(response.text)
 
-#Address
+# Address
 address = soup.find("meta", property="og:zillow_fb:address")
 address['content']
-#Beds
+# Beds
 beds = soup.find("meta", property="zillow_fb:beds")
 beds['content']
-#Baths
+# Baths
 baths = soup.find("meta", property="zillow_fb:baths")
 baths['content']
-#Description
+# Description
 description = soup.find("meta", property="zillow_fb:description")
 description['content']
-#Price
+# Price
 price = soup.find("meta", property="product:price:amount")
-price['content']
-#JSON
-data = json.loads(soup.find("script", type="application/ld+json").text)
+# price['content']
+# JSON
+data = json.loads(soup.find("script", type="application/ld+json").string)
 data.keys()
 data['@type']
-data['name'] #Same as address
-data['floorSize']['value'] #Sq. ft.
-data['numberOfRooms'] #Same as bedrooms
-data['address'] #Already broken down into pieces
-data['geo'] #Latitude and longitude
-#Bigger JSON
+data['name']  # Same as address
+data['floorSize']['value']  # Sq. ft.
+data['numberOfRooms']  # Same as bedrooms
+data['address']  # Already broken down into pieces
+data['geo']  # Latitude and longitude
+# Bigger JSON
 
-data = json.loads(soup.find("script", type="application/json", id="hdpApolloPreloadedData").text)
+data = json.loads(soup.find("script", type="application/json",
+                  id="hdpApolloPreloadedData").string)
 data = json.loads(data['apiCache'])
-data.keys() #The keys got really wonky
-# dict_keys(['VariantQuery{"zpid":81444371,"altId":null}', 
+data.keys()  # The keys got really wonky
+# dict_keys(['VariantQuery{"zpid":81444371,"altId":null}',
 #            'ForSaleDoubleScrollFullRenderQuery{"zpid":81444371,
 #            "contactFormRenderParameter":{"zpid":81444371,"platform":"desktop","isDoubleScroll":true}}'])
 
-#LOTS of good data
+# LOTS of good data
 data['VariantQuery{"zpid":81444371,"altId":null}']['property'].keys()
 data['VariantQuery{"zpid":81444371,"altId":null}']['property']['yearBuilt']
 data['VariantQuery{"zpid":81444371,"altId":null}']['property']['lotSize']
